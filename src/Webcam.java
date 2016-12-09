61f49ceb7d49f468f8570a7dd0ccb86eaffb8f1b
Kevin McKay <kmckay@bensalemsd.org>
2016-12-09 14:36:04 -0500

    Update Webcam.java

--- src/Webcam.java
+++ src/Webcam.java
@@ -3,7 +3,11 @@ import org.opencv.core.Mat;
 //import org.opencv.highgui.VideoCapture;
 import org.opencv.videoio.*;
 import org.opencv.imgcodecs.Imgcodecs;
-import java.io.IOException;
+//import java.io.IOException;
+
+//Running on Raspberry Pi 3 with Pi Camera module requires adding the camera module to the kernel.
+//This is done with the command $sudo modprobe bcm2835-v4l2
+//Sending this to the command line was removed here because its better done through a shell script than invoking sudo within the code
 
 public class Webcam {
 
@@ -14,6 +18,8 @@ public class Webcam {
     System.loadLibrary("opencv_java310");
     String OS = System.getProperty("os.name");
     System.out.println("x" + OS + "x");
+/*  //Following lines are an internal activation of the PiCamera module within linux.
+    //Removing this and adding a note above because this is better accomplished through shell scripting
     if(OS.contains("inu")){
         System.out.println("Linux");
         Runtime rt = Runtime.getRuntime();
@@ -27,7 +33,7 @@ public class Webcam {
     else{
         System.out.println("Not Linux?");
     }
-
+*/
     VideoCapture camera = new VideoCapture(0);
     try{
         Thread.sleep(1500);
